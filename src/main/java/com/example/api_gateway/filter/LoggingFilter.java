b69d1a75094a434611a8d934a866c31beea589a1
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-05-14 12:45:28 -0700

    first commit-implementing api-gateway for the transport-company-app

---
+++ src/main/java/com/example/api_gateway/filter/LoggingFilter.java
@@ -0,0 +1,30 @@
+//package com.example.api_gateway.filter;
+//
+//import jakarta.servlet.*;
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.IOException;
+//
+//@Component
+//public class LoggingFilter implements Filter {
+//
+//    @Override
+//    public void doFilter(
+//            ServletRequest request,
+//            ServletResponse response,
+//            FilterChain chain
+//    ) throws IOException, ServletException {
+//
+//        HttpServletRequest req = (HttpServletRequest) request;
+//
+//        System.out.println(
+//                "GATEWAY REQUEST => "
+//                        + req.getMethod()
+//                        + " "
+//                        + req.getRequestURI()
+//        );
+//
+//        chain.doFilter(request, response);
+//    }
+//}
